9b15c09b18e3db3c46cd56554f63c6ea7ea77f05
David Brown <djb_qwerty@hotmail.com>
2018-10-28 21:02:48 +1100

    more print output

--- libsrc/libsph/calculate_particle_energy_deriv_interior.c
+++ libsrc/libsph/calculate_particle_energy_deriv_interior.c
@@ -418,9 +418,9 @@ void *CalculateParticleEnergyDeriv_INTERIOR(void *v_td)
 /*  djb start */
       if (rank ==2)
       {
-        if ( (i>470-10)  && (i < 470+10) )
+        if ( (i=470)  && (j = 474) )
         {
-          printf("here 20:   i = %i j = %i fact_h0 = %20.10f fact_h2  = %20.10f fact_v = %20.10f sum_v = %20.10f \n",i,j,fact_h0,fact_h2,fact_v,sum_v);
+          printf("here 21:   i = %i j = %i fact_h0 = %20.10f m_j = %20.10f p_i = %20.10f p_j = %20.10f rho2_i = %20.10f rho2_j = %20.10f PI_ij = %20.10f \n",i,j,fact_h0,m_j,p_i,p_j,rho2_i,rho2_j,PI_ij);
         }
       }
 /* djb end */
