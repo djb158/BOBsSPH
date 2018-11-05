05368de1bc36980e7c6981e95c9c0f9863f2aa32
David Brown <djb_qwerty@hotmail.com>
2018-11-05 22:43:52 +1100

    more prints

--- libsrc/libsph/integrate_particle_velocity_boundary.c
+++ libsrc/libsph/integrate_particle_velocity_boundary.c
@@ -68,6 +68,8 @@ int IntegrateParticleVelocity_BOUNDARY(double **x,PARS *pars)
 #include "pars_include.h"
   verbosity(1,fname,pars);
 
+  printf(" HERE 3002 x = %20.10f \n",x[0][9195]);
+
   nparticles = node_info[rank].nparticles;
   n_boundary = node_info[rank].n_boundary;
 
@@ -218,6 +220,70 @@ int IntegrateParticleVelocity_BOUNDARY(double **x,PARS *pars)
     }
   }
 
+  printf(" HERE 3001 x = %20.10f \n",x[0][9195]);
+
+  n_ghost = node_info[rank].n_ghost;
+  for (index=0;index<n_ghost;index++)
+  {
+    l            = node_info[rank].ghost[index];
+    species      = node_info[rank].species[l];
+    i_mirror_raw = node_info[rank].ghost_mirror[index];
+    l_mirror     = node_info[rank].inv_raw_index[i_mirror_raw];
+
+    switch(species)
+    {
+/*                                                                           */
+/*                 LHS boundary                                              */
+/*                                                                           */
+      case 1025:
+        x[0][l]  =  X0 - (x[0][l_mirror] - X0);
+        x[1][l]  =        x[1][l_mirror];
+        x[2][l]  =        x[2][l_mirror];
+        break;
+/*                                                                           */
+/*                 RHS boundary                                              */
+/*                                                                           */
+      case 1026:
+        x[0][l]  =  X1 - (x[0][l_mirror] - X1);
+        x[1][l]  =        x[1][l_mirror];
+        x[2][l]  =        x[2][l_mirror];
+        break;
+/*                                                                           */
+/*                 BOTTOM boundary                                           */
+/*                                                                           */
+      case 1027:
+        x[0][l]  =        x[0][l_mirror];
+        x[1][l]  =        x[1][l_mirror];
+        x[2][l]  =  Z0 - (x[2][l_mirror] - Z0);
+        break;
+/*                                                                           */
+/*                 TOP boundary                                              */
+/*                                                                           */
+      case 1028:
+        x[0][l]  =        x[0][l_mirror];
+        x[1][l]  =        x[1][l_mirror];
+        x[2][l]  =  Z1 - (x[2][l_mirror] - Z1);
+        break;
+/*                                                                           */
+/*                 FRONT boundary                                            */
+/*                                                                           */
+      case 1029:
+        x[0][l]  =        x[0][l_mirror];
+        x[1][l]  =  Y0 - (x[1][l_mirror] - Y0);
+        x[2][l]  =        x[2][l_mirror];
+        break;
+/*                                                                           */
+/*                 BACK boundary                                             */
+/*                                                                           */
+      case 1030:
+        x[0][l]  =        x[0][l_mirror];
+        x[1][l]  =  Y1 - (x[1][l_mirror] - Y1);
+        x[2][l]  =        x[2][l_mirror];
+        break;
+    }
+  }
+
+  printf(" HERE 3000 x = %20.10f \n",x[0][9195]);
   exit_status = EXIT_SUCCESS;
 
 RETURN:
