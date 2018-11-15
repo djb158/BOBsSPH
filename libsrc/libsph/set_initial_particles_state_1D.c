203e35dc5978f22291adb511ae61e0fd81627bd3
David Brown <djb_qwerty@hotmail.com>
2018-11-15 21:21:18 +1100

    moved particle initialization to set_initial_particle_state from create_particles

--- libsrc/libsph/set_initial_particles_state_1D.c
+++ libsrc/libsph/set_initial_particles_state_1D.c
@@ -277,11 +277,21 @@ PARTICLES *SetInitialParticlesState1D(PARS *pars,int particles_num,char *rank_na
     if ( (raw_index>=(rank+0)*NPARTICLES) && (raw_index<(rank+1)*NPARTICLES) )
     {
       particles[0].m[nparticles]                = PARTICLE_MASS;
+      particles[0].h[nparticles]                = 0.0;
       particles[0].x[0][nparticles]             = raw_particle[0].x[0][t];
       particles[0].x[1][nparticles]             = raw_particle[0].x[1][t];
-      particles[0].x[2][nparticles]             = raw_particle[0].x[2][t];
+      particles[0].x[2][nparticles]             = raw_particle[0].x[2][t]; 
+      particles[0].v[0][nparticles]             = 0.0;
+      particles[0].v[1][nparticles]             = 0.0;
+      particles[0].v[2][nparticles]             = 0.0;
+      particles[0].dvdt[0][nparticles]          = 0.0;
+      particles[0].dvdt[1][nparticles]          = 0.0;
+      particles[0].dvdt[2][nparticles]          = 0.0;
+      particles[0].p[nparticles]                = 0.0;
       particles[0].rho[nparticles]              = raw_particle[0].rho[t];
       particles[0].U[nparticles]                = raw_particle[0].U[t]; 
+      particles[0].dUdt[nparticles]             = 0.0;
+      particles[0].divV[nparticles]             = 0.0;
       particles[0].raw_index[nparticles]        = raw_index;
       node_info[rank].raw_index[nparticles]     = raw_index;
       node_info[rank].inv_raw_index[raw_index]  = nparticles;
