b1be0fb6c6d909203dfe7737b1b49922e203a569
David Brown <djb_qwerty@hotmail.com>
2019-05-30 22:11:05 +1000

    changed start point for token_r

--- libsrc/libsph/set_initial_particles_state_2D.c
+++ libsrc/libsph/set_initial_particles_state_2D.c
@@ -268,7 +268,7 @@ PARTICLES *SetInitialParticlesState2D(PARS *pars,int particles_num,char *rank_na
   printf(" maxx = %20.10f \n",maxx);
   printf(" HERE 4 \n");
   TOTAL_PARTICLES = raw_index; 
-  NPARTICLES = TOTAL_PARTICLES/(cluster_size-1);
+  NPARTICLES = TOTAL_PARTICLES/cluster_size;
   printf(" val1: %i   val2: %i \n",(TOTAL_PARTICLES-NPARTICLES*(cluster_size-1) + (cluster_size-1)*NPARTICLES),NPARTICLES);
   valid_values = (double)(TOTAL_PARTICLES + cluster_size*PARTICLES_IN_Z)/(double)(cluster_size+1);
   printf(" valid_values = %20.10f other = %20.10f \n",valid_values,(double)NPARTICLES);
